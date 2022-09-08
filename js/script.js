42e427886c50bf6273e51d34f42e60d6a90b2135
zacmarcengill <zacmarcengill@gmail.com>
2022-09-08 12:49:39 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// console.log('hello');
+
+// // function for welcome() ... includes needed info and instructions.
+
+// function to build ship as word is completed or guessed right
+
+// function to hide letters of word and toggle them on when guess right
+
+// function for guessed letter input box with guess button
+
+// function to add guessed letters to display box
+
+// class with words to guess in array
+
+// class spaceShip
+
+// class hiddenWord or hiddenLetters
+
+// class guessedLetter
+
+// function for playGame
+// class hiddenWords
+//constructor (array, length)
+
+// const gameSettings
+// playerName:
+// numOfWordRounds: 5
+// numOfShipParts = NumOfWordRounds
+
+// let player =
+
+// // function for each word to be hidden/guessed {
+//     //let firstWordArr = [B, E, L, I, E, V, E]
+//     // const letter = document.getElementByID("firstWordFirstLetter");
+//     // if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+
+// //
