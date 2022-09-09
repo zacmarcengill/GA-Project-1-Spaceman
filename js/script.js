3a05b6b3857b577f9e04eb9eacc0a32909e91880
zacmarcengill <zacmarcengill@gmail.com>
2022-09-09 11:22:57 -0500

    Update with fresh start

--- js/script.js
+++ js/script.js
@@ -1,4 +1,73 @@
-// console.log('hello');
+/*----- constants -----*/
+
+const alphabetLtrs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+
+//// Game Setup //// 
+
+// function playGame() {
+// 	class game {
+// 		words = 'DEFAULT';
+
+// 		constructor(words) {
+// 			this.words = words;
+// 		}
+// 	}
+// }
+
+// const round1 = function () {
+//     return this.word
+// }
+ 
+// const firstWord = new game.words('mountain');
+
+// console.log(firstWord);
+////// User Stories  ///////
+
+//// MVP#1: Game Welcome ////
+// alert within div that Welcomes player and gives them instructions about how to play game and what to expect
+
+//// MVP#2: Hidden Word to be Guessed ////
+// Break one word down into an array of it's numbers
+
+// display the array of number in individual divs with a div containing a "block" that will be visible to cover each letter until they are guessed and will then disappear revealing the number
+
+// start with one word/number length...build from there
+
+// create a class for the hidden word to be guessed with defaults that allows multiple words to be added later
+
+// scale the "word class" to auto size to different word lengths using array[i.length] or something similar
+
+//// MVP #3: Visual Feedback After Guessing Letter ////
+// input box where letter is typed with button to the side that when clicked either 1) reveals that correct letter in the word or 2) adds letter to placed div at bottom of screen labeled "Guessed Letters"
+
+// alert displays "You're Right!" for correct letter or "Good Guess." for wrong letter guess.
+
+//// MVP #4: Ship Gets Built If Word Is Completed ////
+// create CSS parts of the ship (body, legs, window, jet, doors)
+
+// hide ship parts in divs until round is won, then ship parts will appear
+
+// display "Game Over", "You Won", and "Want To Play Again" prompts
+
+////  Stretch Goals ////
+// Provide hints as user gets letters wrong.
+
+// create countdown clock that displays time remaining to guess word...then "Times Up - Game Over"
+
+// Create Multiple levels of greater complexity to build other ships in further rounds of game
+
+// create animation of ship flying away when round is won and ship is built.
+
+//////// Psuedocode ///////////
 
 // // function for welcome() ... includes needed info and instructions.
 
@@ -39,3 +108,25 @@
 //     }
 
 // //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////// Console Spaceman /////////////////////
+
